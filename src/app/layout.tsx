6676c3336ed517666b56455371f9fd13a703fe3d
Vishal-Kamath <vishalkamath853@gmail.com>
2023-12-27 16:21:43 +0530

    ➕Add: base lyout design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/utils/lib";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -15,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body
+        className={cn(montserrat.className, "flex flex-col gap-3 px-padding")}
+      >
+        <header className="py-4">
+          <h1 className="text-2xl font-bold text-indigo-800">todo</h1>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
