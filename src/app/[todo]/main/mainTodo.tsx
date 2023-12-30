50e2e688f38fba9ce79951a606baea411f19215f
Vishal-Kamath <vishalkamath853@gmail.com>
2023-12-30 14:06:37 +0530

    ➕Add: todo responsive

--- src/app/[todo]/main/mainTodo.tsx
+++ src/app/[todo]/main/mainTodo.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { Todo } from "@/hooks/useTodo";
-import { FC } from "react";
+import { FC, HTMLAttributes } from "react";
 import MainTodoList from "./mainTodoList";
+import { cn } from "@/utils/lib";
 
-const MainTodo: FC<{
+interface Props extends HTMLAttributes<HTMLDivElement> {
   todos: Todo[];
   setTodos: (updatedTodos: Todo[]) => void;
-}> = ({ todos, setTodos }) => {
+}
+const MainTodo: FC<Props> = ({ todos, setTodos, className, ...props }) => {
   return (
-    <section className="flex w-full flex-col gap-6 p-6">
+    <section
+      className={cn("flex w-full flex-col gap-6 p-6", className)}
+      {...props}
+    >
       <h1 className="text-4xl font-semibold text-neutral-600">Tasks</h1>
 
       <MainTodoList todos={todos} setTodos={setTodos} />
--- src/app/[todo]/page.tsx
+++ src/app/[todo]/page.tsx
@@ -3,13 +3,23 @@
 import SubTodo from "./sub/subTodo";
 import MainTodo from "./main/mainTodo";
 import { useTodos } from "@/hooks/useTodo";
+import { cn } from "@/utils/lib";
 
 export default function Home({ params }: { params: { todo: string } }) {
   const [todos, setTodos] = useTodos("home");
 
   return (
-    <main className="grid w-full grid-cols-2">
-      <MainTodo todos={todos} setTodos={setTodos} />
+    <main
+      className={cn(
+        "grid w-full grid-cols-1",
+        params.todo !== "home" && "lg:grid-cols-2",
+      )}
+    >
+      <MainTodo
+        todos={todos}
+        setTodos={setTodos}
+        className={params.todo !== "home" ? "max-lg:hidden" : ""}
+      />
       {params.todo !== "home" ? (
         <SubTodo
           belongsTo={params.todo}
