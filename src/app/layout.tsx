7ee6bfd53f588f6e97d1e575f16db9cfc6b324f9
Vishal-Kamath <vishalkamath853@gmail.com>
2023-12-30 15:33:06 +0530

    ➕Add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,29 @@ import Header from "@/components/header/header";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Todo App",
+  title: "todo",
+
+  // Favicons
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png", type: "image/png" }],
+    other: [
+      {
+        url: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
